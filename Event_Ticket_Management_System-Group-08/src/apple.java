03333f55b89734428c58adc98cf3bd3261d89d43
IT25100562 <it25100562@my.sliit.lk>
2026-03-23 14:29:21 +0530

    Initial workspace
    Corrected project file to begin coding

--- Event_Ticket_Management_System-Group-08/src/apple.java
+++ Event_Ticket_Management_System-Group-08/src/apple.java
@@ -1,5 +1,6 @@
 public class apple {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main (String [] args){
+        System.out.print("Hello");
     }
+
 }
